4296d0a4707e727c470f81934072fab0795135da
Siddarth Balaji <sid.balaji@berkeley.edu>
2022-11-13 13:52:18 -0400

    commit tree c1ee9c17dde280f87894c42e1625087158a096fb

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Write your solution here!
+var cats = ['Milo','Otis','Garfield'];
+
+function destructivelyAppendCat(cat){
+   return cats.push(cat)
+}
+
+function destructivelyPrependCat(cat){
+    return cats.unshift(cat)
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop()
+    return cats
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift()
+    return cats
+}
+
+function appendCat(name){
+    return [...cats,name]
+}
+
+function prependCat(name){
+    return [name, ...cats]
+}
+
+function removeLastCat(name){
+    return cats.slice(0,-1)
+}
+
+function removeFirstCat(name){
+    return cats.slice(1,)
+}
+
